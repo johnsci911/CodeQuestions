b0c58bb30e25fa1b7eb0fe72e878f262b47abdf2
jkcachero <johnguitarizta@gmail.com>
2023-05-19 23:26:00 +0800

    Testing ideas

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -14,7 +14,7 @@ class IdeaController extends Controller
     public function index()
     {
         return view('idea.index', [
-            'ideas' => Idea::simplePaginate(10),
+            'ideas' => Idea::simplePaginate(Idea::PAGINATION_COUNT),
         ]);
     }
 
