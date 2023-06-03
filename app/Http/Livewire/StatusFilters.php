f510280eaa56807b3648b9f06ebe6df2d4157f3e
jkcachero <johnguitarizta@gmail.com>
2023-06-04 01:42:40 +0800

    Category Filters

--- app/Http/Livewire/StatusFilters.php
+++ app/Http/Livewire/StatusFilters.php
@@ -2,47 +2,45 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Idea;
 use App\Models\Status;
 use Illuminate\Support\Facades\Route;
 use Livewire\Component;
 
 class StatusFilters extends Component
 {
-    public $status = 'All';
+    public $status;
     public $statusCount;
-
-    protected $queryString = [
-        'status',
-    ];
 
     public function mount()
     {
         $this->statusCount = Status::getCount();
+        $this->status = request()->status ?? 'All';
 
         if (Route::currentRouteName() === 'idea.show') {
             $this->status = null;
-            $this->queryString = [];
         }
     }
 
     public function setStatus($newStatus)
     {
         $this->status = $newStatus;
+        $this->emit('queryStringUpdatedStatus', $this->status);
 
-        // if ($this->getPreviousRouteName() === 'idea.show') {
+        if ($this->getPreviousRouteName() === 'idea.show') {
             return redirect()->route('idea.index', [
-                'status' => $this->status
-            ]);
-        // }
-    }
-
-    private function getPreviousRouteName()
-    {
-        return app('router')->getRoutes()->match(app('request')->create(url()->previous()))->getName();
+                    'status' => $this->status,
+                ]);
+        }
     }
 
     public function render()
     {
         return view('livewire.status-filters');
+    }
+
+    public function getPreviousRouteName()
+    {
+        return app('router')->getRoutes()->match(app('request')->create(url()->previous()))->getName();
     }
 }
--- resources/views/livewire/ideas-index.blade.php
+++ resources/views/livewire/ideas-index.blade.php
@@ -1,11 +1,11 @@
 <div>
     <div class="flex flex-col space-y-4 filters md:flex-row md:space-x-6 md:space-y-0">
         <div class="w-full px-4 md:px-0 md:w-1/3">
-            <select id="category" name="category" class="w-full px-4 py-2 border-none rounded-xl">
-                <option value="Category1">Category One</option>
-                <option value="Category2">Category Two</option>
-                <option value="Category3">Category Three</option>
-                <option value="Category4">Category Four</option>
+            <select wire:model="category" id="category" name="category" class="w-full px-4 py-2 border-none rounded-xl">
+                <option value="All Categories">All Categories</option>
+                @foreach ($categories as $category)
+                    <option value="{{ $category->name }}">{{ $category->name }}</option>
+                @endforeach
             </select>
         </div>
 
