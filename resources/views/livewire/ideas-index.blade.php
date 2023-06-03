89ca192a0d03d0b45fb88e073ecbc93539be2e6a
jkcachero <johnguitarizta@gmail.com>
2023-06-03 18:43:15 +0800

    User livewire component to fetch ideas

---
+++ resources/views/livewire/ideas-index.blade.php
@@ -0,0 +1,44 @@
+<div>
+    <div class="flex flex-col space-y-4 filters md:flex-row md:space-x-6 md:space-y-0">
+        <div class="w-full px-4 md:px-0 md:w-1/3">
+            <select id="category" name="category" class="w-full px-4 py-2 border-none rounded-xl">
+                <option value="Category1">Category One</option>
+                <option value="Category2">Category Two</option>
+                <option value="Category3">Category Three</option>
+                <option value="Category4">Category Four</option>
+            </select>
+        </div>
+
+        <div class="w-full px-4 md:px-0 md:w-1/3">
+            <select id="other_filters" name="other_filters" class="w-full px-4 py-2 border-none rounded-xl">
+                <option value="Filter1">Filter One</option>
+                <option value="Filter2">Filter Two</option>
+                <option value="Filter3">Filter Three</option>
+                <option value="Filter4">Filter Four</option>
+            </select>
+        </div>
+
+        <div class="relative w-full px-4 md:px-0 md:w-2/3">
+            <div class="absolute top-0 flex items-center h-full ml-2">
+                <svg class="w-4 text-gray-500" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                </svg>
+            </div>
+            <input type="search" placeholder="Find an idea"
+                class="w-full px-4 py-2 pl-8 bg-white border-none rounded-xl placeholder:text-gray-700">
+        </div>
+    </div> <!-- end filters -->
+
+    <div class="mx-4 my-6 space-y-6 ideas-container md:mx-0">
+        @foreach ($ideas as $idea)
+            <livewire:idea-index :key="$idea->id" :idea="$idea" :votesCount="$idea->votes_count" />
+        @endforeach
+
+        <div class="my-8">
+            {{ $ideas->links() }}
+        </div>
+    </div> <!-- end ideas-container -->
+</div>
+
--- tests/Feature/VoteIndexPageTest.php
+++ tests/Feature/VoteIndexPageTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Http\Livewire\IdeaIndex;
+use App\Http\Livewire\IdeasIndex;
 use App\Models\Category;
 use App\Models\Idea;
 use App\Models\Status;
@@ -38,7 +39,7 @@ class VoteIndexPageTest extends TestCase
     }
 
     /** @test */
-    public function index_page_correcly_receive_votes_count()
+    public function index_index_livewire_component_correcly_receive_votes_count()
     {
         $userOne = User::factory()->create();
         $userTwo = User::factory()->create();
@@ -65,7 +66,7 @@ class VoteIndexPageTest extends TestCase
             'user_id' => $userTwo->id,
         ]);
 
-        $this->get(route('idea.index'))
+        Livewire::test(IdeasIndex::class)
             ->assertViewHas('ideas', function($ideas) {
                 return $ideas->first()->votes_count == 2;
             });
@@ -119,13 +120,12 @@ class VoteIndexPageTest extends TestCase
             'user_id' => $user->id,
         ]);
 
-        $response = $this->actingAs($user)->get(route('idea.index'));
-
-        $ideaWithVotes = $response['ideas']->items()[0];
+        $idea->votes_count = 1;
+        $idea->voted_by_user = 1;
 
         Livewire::actingAs($user)
             ->test(IdeaIndex::class, [
-                'idea' => $ideaWithVotes,
+                'idea' => $idea,
                 'votesCount' => 5,
             ])
             ->assertSet('hasVoted', true)
@@ -207,13 +207,12 @@ class VoteIndexPageTest extends TestCase
             'user_id' => $user->id,
         ]);
 
-        $response = $this->actingAs($user)->get(route('idea.index'));
-
-        $ideaWithVotes = $response['ideas']->items()[0];
+        $idea->votes_count = 1;
+        $idea->voted_by_user = 1;
 
         Livewire::actingAs($user)
             ->test(IdeaIndex::class, [
-                'idea' => $ideaWithVotes,
+                'idea' => $idea,
                 'votesCount' => 5,
             ])
             ->call('vote')
