2b3493731304c9bb28ff0fe68e52e63fcd48e4fd
jkcachero <johnguitarizta@gmail.com>
2023-06-07 08:49:42 +0800

    Use job to handle email notifications

---
+++ app/Jobs/NotifyAllVoters.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\IdeaStatusUpdatedMailable;
+use App\Models\Idea;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class NotifyAllVoters implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $idea;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(Idea $idea)
+    {
+        $this->idea = $idea;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $this->idea->votes()
+            ->select('name', 'email')
+            ->chunk(100, function ($voters) {
+                foreach ($voters as $user) {
+                    Mail::to($user)
+                        ->queue(new IdeaStatusUpdatedMailable($this->idea));
+                }
+            });
+    }
+}
