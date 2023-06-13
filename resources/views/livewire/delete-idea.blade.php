765b1d47c5b54de13d4f5846c431a0d3de3b4e93
jkcachero <johnguitarizta@gmail.com>
2023-06-13 15:30:44 +0800

    Refactor modals: 1

--- resources/views/livewire/delete-idea.blade.php
+++ resources/views/livewire/delete-idea.blade.php
@@ -1,54 +1,8 @@
-<div
-    x-cloak
-    x-data="{ isOpen: false }"
-    x-show="isOpen"
-    x-init="window.livewire.on('ideaWasDeleted', () => {
-        isOpen = false
-    })"
-    @keydown.escape.window="isOpen = false"
-    @custom-show-delete-modal.window="
-        isOpen = true
-        $nextTick(() => $refs.confirmButton.focus())
-    "
-    class="relative z-10"
-    aria-labelledby="modal-title"
-    role="dialog"
-    aria-modal="true"
->
-    <div
-        x-show="isOpen"
-        x-transition.opacity.duration.400ms
-        class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
-    ></div>
-
-    <div
-        x-show="isOpen"
-        x-transition.duration.400ms.ease-in-out
-        class="fixed inset-0 z-10 overflow-y-auto"
-    >
-        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-            <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                <div class="sm:flex sm:items-start">
-                    <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                    <svg class="h-6 w-6 text-red" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
-                    </svg>
-                    </div>
-                    <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                    <h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title">Delete Idea</h3>
-                    <div class="mt-2">
-                        <p class="text-sm text-gray-500">Are you sure you want to delete this idea? This action cannot be undone.</p>
-                    </div>
-                    </div>
-                </div>
-                </div>
-                <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                    <button wire:click="deleteIdea" x-ref="confirmButton" type="button" class="inline-flex w-full justify-center rounded-md bg-blue px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-hover focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue sm:ml-3 sm:w-auto">Delete</button>
-                    <button @click="isOpen = false" type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue sm:mt-0 sm:w-auto">Cancel</button>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
+<x-modal-confirm 
+    event-to-open-modal="custom-show-delete-modal"
+    event-to-close-modal="ideaWasDeleted"
+    modal-title="Delete Idea"
+    modal-description="Are you sure you want to delete this idea? This action cannot be undone."
+    modal-confirm-button-text="Delete"
+    wire-click="deleteIdea"
+/>
