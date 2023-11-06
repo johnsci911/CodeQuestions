53afcf7e27c4126beba6a05ea798d2f6296d670d
jkcachero <johnguitarizta@gmail.com>
2023-11-07 02:45:50 +0800

    Format

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,9 +17,9 @@
             <x-input-label for="password" :value="__('Password')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+                          type="password"
+                          name="password"
+                          required autocomplete="current-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
